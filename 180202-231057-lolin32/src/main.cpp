b75430bb912878fdd8e6f4303ddd5fe7ac8f6bc7
Alexander Tarancon <ataranco@umail.iu.edu>
2018-02-05 15:09:21 -0500

    Deleted Unneded code

--- 180202-231057-lolin32/src/main.cpp
+++ 180202-231057-lolin32/src/main.cpp
@@ -15,39 +15,42 @@ OLED oled = OLED(GPIO_NUM_4, GPIO_NUM_5, SSD1306_128x64);
 
 void myTask(void *pvParameters) {
 
-	ostringstream os;
-//	char *data = (char*) malloc(32);
 
-//	for(uint8_t i=0;i<64;i++){
-//		oled.clear();
-//		oled.draw_hline(0, i, 100, WHITE);
-//		sprintf(data,"%d",i);
-//		oled.draw_string(105, 10, string(data), BLACK, WHITE);
-//		oled.refresh(false);
-//		vTaskDelay(1000 / portTICK_PERIOD_MS);
-//	}
+	ostringstream os;
+
+
 	while (1) {
+
+		// clear oled screen
+
 		oled.clear();
 
-		// put name
+		// initialize string  stream
 		os.str("");
 
-		os << "**Name**" ;
+		// append string to stream
+		os << "Hello World" ;
 
-
+   // select font type
 		oled.select_font(1);
 
+   // draw string
 		oled.draw_string(0, 0, os.str(), WHITE, BLACK);
 
+		// refresh
 		oled.refresh(true);
 
+
+    // delay one second
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-		// Added
+		// clear screen
 		oled.clear();
 
+    // refresh screen
 		oled.refresh(true);
 
+   // delay one second
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 
 
@@ -64,19 +67,8 @@ void app_main() {
 	if (oled.init()) {
 
 		ESP_LOGI("OLED", "oled inited");
-//		oled.draw_rectangle(10, 30, 20, 20, WHITE);
-//		oled.select_font(0);
-//		//deprecated conversion from string constant to 'char*'
-//		oled.draw_string(0, 0, "glcd_5x7_font_info", WHITE, BLACK);
-//		ESP_LOGI("OLED", "String length:%d",
-//				oled.measure_string("glcd_5x7_font_info"));
-//		oled.select_font(1);
-//		oled.draw_string(0, 18, "tahoma_8pt_font_info", WHITE, BLACK);
-//		ESP_LOGI("OLED", "String length:%d",
-//				oled.measure_string("tahoma_8pt_font_info"));
-//		oled.draw_string(55, 30, "Hello ESP32!", WHITE, BLACK);
-//		oled.refresh(true);
-//		vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+
 		xTaskCreatePinnedToCore(&myTask, "Name", 2048, NULL, 5, NULL, 1);
 	} else {
 		ESP_LOGE("OLED", "oled init failed");
