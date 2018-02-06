222268256444aa80b4400aa74b69683b71f54a39
Alexander Tarancon <ataranco@umail.iu.edu>
2018-02-06 12:54:03 -0500

    Formatting code

--- 180202-231057-lolin32/src/main.cpp
+++ 180202-231057-lolin32/src/main.cpp
@@ -16,43 +16,44 @@ OLED oled = OLED(GPIO_NUM_4, GPIO_NUM_5, SSD1306_128x64);
 void myTask(void *pvParameters) {
 
 
-		ostringstream os;
+								ostringstream os;
 
 
-	while (1) {
+								while (1) {
 
-		// clear oled screen
-		 oled.clear();
+																// clear oled screen
+																oled.clear();
 
-  os.str("hello World");
-// Display String
-
-// select font type
- oled.select_font(1);
-
-   // draw string
-		oled.draw_string(0, 0, os.str() , WHITE, BLACK);
-
-		// refresh
-		oled.refresh(true);
+																// Display string
+																os.str("hello World");
 
 
-    // delay one second
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+																// select font type
+																oled.select_font(1);
 
- // Clear O.L.E.D
+																// draw string
+																oled.draw_string(0, 0, os.str(), WHITE, BLACK);
 
-		// clear screen
-		oled.clear();
-
-    // refresh screen
-		oled.refresh(true);
-
-   // delay one second
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+																// refresh
+																oled.refresh(true);
 
 
-	}
+																// delay number of ticks
+																vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+																// Clear O.L.E.D
+
+																// clear screen
+																oled.clear();
+
+																// refresh screen
+																oled.refresh(true);
+
+																// delay number of ticks
+																vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+
+								}
 
 }
 
@@ -63,22 +64,22 @@ void app_main() {
 
 
 // create oled object
-	oled = OLED(GPIO_NUM_4, GPIO_NUM_5, SSD1306_128x64);
+								oled = OLED(GPIO_NUM_4, GPIO_NUM_5, SSD1306_128x64);
 
-	//Verifiy object created
+								//Verifiy object created
 
-	if (oled.init()) {
+								if (oled.init()) {
 
-		ESP_LOGI("OLED", "oled inited");
+																ESP_LOGI("OLED", "oled inited");
 
 
 
-     // create a task
-		xTaskCreatePinnedToCore(&myTask, "Name", 2048, NULL, 5, NULL, 1);
+																// create a task
+																xTaskCreatePinnedToCore(&myTask, "Name", 2048, NULL, 5, NULL, 1);
 
-	} else {
-		ESP_LOGE("OLED", "oled init failed");
-	}
+								} else {
+																ESP_LOGE("OLED", "oled init failed");
+								}
 }
 #ifdef __cplusplus
 }
